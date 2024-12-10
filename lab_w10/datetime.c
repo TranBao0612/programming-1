5bff3e044fd915f580aa0cab5ddffb7db33a1c78
TranBao0612 <155043580+TranBao0612@users.noreply.github.com>
2024-12-10 13:54:41 +0000

    lab w10 ex2 complete

--- lab_w10/datetime.c
+++ lab_w10/datetime.c
@@ -1,13 +1,22 @@
 #include <time.h>
+#include <stdio.h>
 
 void sprint_time(char* outstr, struct tm* info)
 {
+    char buffer[50];
+    sprintf(buffer, outstr, info->tm_hour, info->tm_min, info->tm_sec);
 
+    printf("%s\n", buffer);
 }
 
 void sprint_date(char* outstr, struct tm* info)
 {
+    char buffer[50];
+    char *weekday[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
+    char *wday = weekday[info->tm_wday];
+    sprintf(buffer, outstr, wday, info->tm_mday, info->tm_mon + 1, info->tm_year + 1900);
 
+    printf("%s\n", buffer);
 }
 
 int main()
@@ -27,5 +36,14 @@ int main()
 
     info->tm_wday; // day of the week: 0 -> Sun, 1 -> Mon, ...
 
+
+    char outstr1[50] = "Current time is: %.2d:%.2d:%.2d";
+    char outstr2[50] = "Today is %s, %.2d/%.2d/%.4d";
+
+    sprint_time(outstr1, info);
+    sprint_date(outstr2, info);
+
+
     return 0;
 }
+
