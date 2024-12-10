2814702c2869b4b2cfb428255ca105c9bac12b1b
TranBao0612 <155043580+TranBao0612@users.noreply.github.com>
2024-12-10 07:39:51 +0000

    Lab w9 done

---
+++ lab_w9/exercise_2/compare_func.c
@@ -0,0 +1,13 @@
+# include "compare_func.h"
+
+// definitions
+int greater(int a, int b)
+{
+    return a > b ? 1 : 0; // ternary operator
+}
+
+
+int less(int a, int b)
+{
+    return a < b ? 1 : 0; // ternary operator
+}
--- lab_w9/exercise_2/compare_func.h
+++ lab_w9/exercise_2/compare_func.h
@@ -1,18 +1,3 @@
-
-
-
 // declarations
 int less(int a, int b);
 int greater(int a, int b);
-
-// definitions
-int greater(int a, int b)
-{
-    return a > b ? 1 : 0; // ternary operator
-}
-
-
-int less(int a, int b)
-{
-    return a < b ? 1 : 0; // ternary operator
-}
